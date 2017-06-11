6868e3877a8f25cc91ba23fb97fd167d620e01d9
autkina5@gmail.com <Autkina123>
2017-06-11 13:16:06 +0200

    Задание 2.1-2.3

--- sandbox/src/main/java/ru/stqa/utk/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/utk/sandbox/MyFirstProgram.java
@@ -15,13 +15,18 @@ public class MyFirstProgram {
 
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+        Point p1 = new Point(2,5);
+        Point p2 =new Point(6,7);
+        System.out.println("Расстояние между двумя точками " + distance(p1,p2));
     }
     public static void hello(String somebody) {
 
         System.out.println("Hello " + somebody + "!");
     }
-
-
+    public static double distance(Point p1, Point p2){
+        double distance = Math.sqrt((p2.x - p1.x)* (p2.x - p1.x)  + (p2.y - p1.y)* (p2.y - p1.y));
+        return distance;
+    }
 }
 
 
---
+++ sandbox/src/main/java/ru/stqa/utk/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.utk.sandbox;
+
+/**
+ * Created by Tonya on 11.06.2017.
+ */
+public class Point {
+
+    double x;
+    double y;
+
+    public Point (double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+}
