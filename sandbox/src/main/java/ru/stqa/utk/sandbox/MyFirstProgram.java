1628051442be02901fd069b0e3b1a974a20fd490
autkina5@gmail.com <Autkina123>
2017-06-11 12:28:27 +0200

    добавлен this

--- sandbox/src/main/java/ru/stqa/utk/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/utk/sandbox/MyFirstProgram.java
@@ -6,27 +6,22 @@ public class MyFirstProgram {
         hello("World");
         hello("Tonia");
         hello("All");
-        double l = 5;
-        System.out.println("Площадь квадрата с одной стороной " + l + "=" + area(l));
 
-        double a = 4;
-        double b = 6;
+        Square s = new Square(5);
 
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        System.out.println("Площадь квадрата с одной стороной " + s.l + "=" + s.area());
+
+        Rectangle r =new Rectangle(4,6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
     }
     public static void hello(String somebody) {
 
-        System.out.println("Hello " + somebody + "!");   
+        System.out.println("Hello " + somebody + "!");
     }
 
-  public static double area (double len){
-      return len*len;
-  }
 
-    public static double area (double a, double b){
-      return a * b;
-    }
 }
 
 
--- sandbox/src/main/java/ru/stqa/utk/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/utk/sandbox/Square.java
@@ -4,7 +4,13 @@ package ru.stqa.utk.sandbox;
  * Created by Tonya on 10.06.2017.
  */
 public class Square {
+public double l;
 
-    double X;
-    double y;
+public Square (double l) {
+this.l=l;
+}
+
+    public double area (){
+        return this.l * this.l;
+    }
 }
