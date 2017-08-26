ff64c069cbf36628645f8b5e16a25be3c29a6fd5
autkina5@gmail.com <Autkina123>
2017-08-26 17:02:49 +0200

    Fixtura

--- addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/GroupCreationTests.java
@@ -21,15 +21,14 @@ public class GroupCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("\\undefined");
         wd.findElement(By.id("content")).click();
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    @Test
+    public void testGroupCreation() {
         wd.findElement(By.id("content")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
