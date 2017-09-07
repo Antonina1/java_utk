84a277a87fb568bec91a299cb62aa6909cc83484
autkina5@gmail.com <Autkina123>
2017-09-07 21:27:08 +0200

    ContactCreation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.utk.addressbook;
+
+public class ContactData {
+    private final String userfirstname;
+    private final String userlastname;
+    private final String nickname;
+    private final String useraddress;
+    private final String usermobile;
+
+    public ContactData(String userfirstname, String userlastname, String nickname, String useraddress, String usermobile) {
+        this.userfirstname = userfirstname;
+        this.userlastname = userlastname;
+        this.nickname = nickname;
+        this.useraddress = useraddress;
+        this.usermobile = usermobile;
+    }
+
+    public String getUserfirstname() {
+        return userfirstname;
+    }
+
+    public String getUserlastname() {
+        return userlastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getUseraddress() {
+        return useraddress;
+    }
+
+    public String getUsermobile() {
+        return usermobile;
+    }
+}
