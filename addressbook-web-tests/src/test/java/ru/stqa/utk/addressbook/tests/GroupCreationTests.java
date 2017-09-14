1b0df074cee1d6a18bc96e0828258ee59e52ecea
autkina5@gmail.com <Autkina123>
2017-09-14 21:53:17 +0200

    Выделяем специализированные классы-помощники

--- addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/tests/GroupCreationTests.java
@@ -7,11 +7,11 @@ public class GroupCreationTests extends TestBased {
 
     @Test
     public void testGroupCreation() {
-        app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("test6", "test7", "test8"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test6", "test7", "test8"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
