93f70c836ef143380a614c2783fab711be5661f5
autkina5@gmail.com <Autkina123>
2017-09-16 23:07:26 +0200

    added GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/utk/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.utk.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.utk.addressbook.model.GroupData;
+
+/**
+ * Created by Tonya on 16.09.2017.
+ */
+public class GroupModificationTests extends TestBased {
+
+    @Test
+public void testGroupModification () {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test6", "test7", "test8"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+
+    }
+}
